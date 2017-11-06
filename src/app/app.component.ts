52d7edb4e05c0ab9907ad636b5ecdf557ecbe61b
JoonasSuutari <suutarijoonas@gmail.com>
2017-11-06 14:06:36 +0200

    Started contact-app project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Contact} from './contact/contact';
+import { ContactService} from './contact/services/contact.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Contact list';
+  contacts: Contact[];
+
+  constructor() {
+    this.contacts = [
+      new Contact(1, 'Joonas', 'Suutari', '0501234567', 'Metsäsaimaankatu', 'Lappeenranta'),
+      new Contact(2, 'Esa', 'Esimerkki', '0502345678', 'Esimerkkikatu', 'Helsinki'),
+      new Contact(3, 'Petteri', 'Punakuono', '0503456789', 'Joulukatu', 'Korvatunturi')
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactListItemComponent } from './contact/contact-list/contact-list-item/contact-list-item.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatStepperModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactListItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
